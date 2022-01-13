9345a8917614b640e5c546e50454a19f3c8d88cf
dmjservices <mrdmjservices@gmail>
2022-01-13 13:01:38 -0800

    Update _PushToMT4Dev.bat

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -11,10 +11,10 @@ setlocal enabledelayedexpansion
 :: Source Directory where Version Control Repository is located
 set SOURCE_DIR="%PATH_DSS_Repo%\FALCON_D"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="%PATH_T2_E%\FALCON_D"
+set DEST_DIR="%PATH_T4_E%\FALCON_D"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 ::ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
 
 
-
+pause
