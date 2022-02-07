90d2282a8fe2ba33020e000ce93cfa8fa3fdafd5
dmjservices <mrdmjservices@gmail>
2022-02-07 09:33:48 -0800

    Update FalconDBacktestAutoLaunch.bat

--- FalconDBacktestAutoLaunch.bat
+++ FalconDBacktestAutoLaunch.bat
@@ -4,4 +4,76 @@ rem *************************************************
 
 ping localhost -n 5
 
-start "1" "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\terminal portable 4" "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\settings_falcond.ini"
+
+start "1" "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\terminal portable 4"
+ 
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_AUDJPY\Falcon_D_AUDJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_AUDNZD\Falcon_D_AUDNZD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_AUDSGD\Falcon_D_AUDSGD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_AUDUSD\Falcon_D_AUDUSD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_CADCHF\Falcon_D_CADCHF_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_CADJPY\Falcon_D_CADJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_CHFJPY\Falcon_D_CHFJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_CHFZAR\Falcon_D_CHFZAR_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURAUD\Falcon_D_EURAUD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURCAD\Falcon_D_EURCAD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURCHF\Falcon_D_EURCHF_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURCZK\Falcon_D_ERUCZK_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURGBP\Falcon_D_EURGBP_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURJPY\Falcon_D_EURJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_EURUSD\Falcon_D_EURUSD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_GBPJPY\Falcon_D_GBPJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_GBPUSD\Falcon_D_GBPUSD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_NZDJPY\Falcon_D_NZDJPY_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_NZDUSD\Falcon_D_NZDUSD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_USDCAD\Falcon_D_USDCAD_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_USDCHF\Falcon_D_USDCHF_1.ini"
+
+
+"C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\config\AUTO_BACKTEST\Falcon_D_USDGBP\Falcon_D_USDGBP_1.ini"
+
+
+
+
+
+Exit
